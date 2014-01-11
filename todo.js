daab1bff972006c6875ef1f728db9ba7803224ba
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-11 03:39:32 +0000

    textarea now clears after submit is clicked

--- todo.js
+++ todo.js
@@ -1,10 +1,5 @@
-// 1. create an ol of to-do items
-// 2. Create a textarea or ol element
-   // 3. Create a textbox where user can enter input, 
-// 3. save entered text in variable (?)
-// 3. Add a button to or around textbox that adds the text entered when "add" is clicked.
+
 // 4. after "add" is clicked, text should clear from textbox
-// 5. after "add" is clicked, text should be added to todo list
 // 5. Add an "x" button alongside each todo item that when clicked removes that item.
 // 6. Create a checkbox to the left of todo items. 
 // 7. If checkbox is selected, the text in the todo item is marked with line­through (strike­through)
@@ -17,9 +12,10 @@ $(document).ready(function () {
     var getText= $("#txtDescription").val();
 
     $( "#list ul" ).append( $( "<li>" ).html(getText) );
-  });
 
-  // $('#list ul').append("<li>test</li>"); 
+    // Clears textbox after submit is clicked
+    $('#txtDescription').val('');
+  });
 
 });
 
