f51a607a803f354a05f950cb9176a4c318aed468
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-11 04:08:39 +0000

    added delete button

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
 
-// 4. after "add" is clicked, text should clear from textbox
 // 5. Add an "x" button alongside each todo item that when clicked removes that item.
 // 6. Create a checkbox to the left of todo items. 
 // 7. If checkbox is selected, the text in the todo item is marked with line­through (strike­through)
@@ -10,12 +9,21 @@ $(document).ready(function () {
   $("#btnSubmit").click(function(){
 
     var getText= $("#txtDescription").val();
+    var listItem = $( "<li>" );
+    var deleteButton = $('<button class="delete">Delete</button>');
 
-    $( "#list ul" ).append( $( "<li>" ).html(getText) );
-
+    $( "#list ul" ).append( listItem.html(getText) );
+    listItem.append(deleteButton);
     // Clears textbox after submit is clicked
     $('#txtDescription').val('');
+    deleteButton.click(function () {
+    $(this).parent().remove();
+    });
   });
+
+  $("#list .delete").click(function() { 
+    $(this).parent().remove();
+});
 
 });
 
