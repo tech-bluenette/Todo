83e279b440f67fd051ddfd80ae7a640a702752a0
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-11 02:53:21 +0000

    added a list

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 // 1. create an ol of to-do items
-// 2. Create a textbox, 
+// 2. Create a textarea or ol element
+   // 3. Create a textbox where user can enter input, 
 // 3. save entered text in variable (?)
 // 3. Add a button to or around textbox that adds the text entered when "add" is clicked.
 // 4. after "add" is clicked, text should clear from textbox
@@ -8,18 +9,15 @@
 // 6. Create a checkbox to the left of todo items. 
 // 7. If checkbox is selected, the text in the todo item is marked with line­through (strike­through)
 
-$('form').submit(function () {
-    if ($('input').val() !== '') {
-        var input_value = $('input').val();
-        $('ul').append('<li>' + input_value + '<a href="">x</a></li>');
-        //$('ul').append('<li>' + input_value '<a href="">x</a></li>');
+//code inspired by http://jsfiddle.net/4VkcF/1/
+$(document).ready(function () {
+    
+$("#btnSubmit").click(function(){
 
-    };
-    $('input').val('');
-    return false;
+var getText= $("#txtDescription").val();
+});
 });
 
-$(document).on('click', 'a', function (e) {
-    e.preventDefault();
-    $(this).parent().remove();
-});
+
+
+
