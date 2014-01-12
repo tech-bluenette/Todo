db3998a751438bc8b5e9d17315dae6a4e31d101c
Lindsey Miller <lindseyandmina@gmail.com>
2014-01-12 00:01:40 +0000

    added strikethrough to list items when checked off

--- todo.js
+++ todo.js
@@ -1,32 +1,66 @@
 
-// 6. Create a checkbox to the left of todo items. 
-// 7. If checkbox is selected, the text in the todo item is marked with line­through (strike­through)
-
-//code inspired by http://jsfiddle.net/4VkcF/1/
+// Below code: "wait until page is done loading so that elements have time to load.""
 $(document).ready(function () {
-    
-  $("#btnSubmit").click(function(){
 
-    var getText= $("#txtDescription").val();
+
+  var initialItems = ["Buy groceries", "Walk the dog", "Make dinner", "Graduate from LL"];
+
+  var createListItem = function(text){
+    // Below creates a list item each time this function is called
     var listItem = $( "<li>" );
     var deleteButton = $('<button class="delete">Delete</button>');
+    var checkBox = $("<input type=checkbox>");
+    // theList matches ALL html elements that are ul and have a parent element that have an id of #list 
+    var theList = $( "#list ul" );
 
-    $( "#list ul" ).append( listItem.html(getText) );
+    // Sets the html value of listItem to be "text"
+    listItem.html(text);
+    // Appends the list item to the list
+    theList.append( listItem );
+    // Append delete button to every generated list item
     listItem.append(deleteButton);
-    // Clears textbox after submit is clicked
+    // Prepend checkbox to every generated list item 
+    listItem.prepend(checkBox);
+
+    // Clears textbox because of empty string
     $('#txtDescription').val('');
+    // "this" refers to deleteButton on line above it. The parent of deleteButton is li, so when delete button is clicked it removes that li item.
     deleteButton.click(function () {
-    $(this).parent().remove();
+      $(this).parent().remove();
     });
+    // when checkbox is clicked it applies the css text-decoration: line-through to the parent of checkbox (that li item)
+    checkBox.click(function () {
+      $(this).parent().css('textDecoration', 'line-through');
+    });
+  };
+
+  for (var i = 0; i < initialItems.length; i++ ) {
+    var item = initialItems[i];
+    createListItem(item);
+  }
+
+  // When the submit button is clicked, the text entered in text area is sent to the createListItem function.
+  $("#btnSubmit").click(function() {
+    var getText= $("#txtDescription").val();
+    createListItem(getText);
   });
 
+  // "this" (and selectors like it) always select the last element in the selector.  So it's referring to .delete here.  So when the delete
+  // element is clicked, it removes the parent of .delete (which would be a list item)
   $("#list .delete").click(function() { 
     $(this).parent().remove();
   });
 
+  // this allows the li items to be sortable
   $(function () {
     $("#sortable").sortable();
-    });
+  });
+
+// $('input[type=checkbox]').on('change',function(){
+//   var input = $(this).parent().find('li');
+//   $(input).css('textDecoration','line-through');
+// });
+
 
 
 });
